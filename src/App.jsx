e631db3c68de113613ff223e12a01a360d735034
Elvio Galeano <elvio.galeano827@gmail.com>
2023-03-30 22:19:00 -0300

    main

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { Route, Routes } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
+import './App.css'
+import Layout from './components/Layout'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Detail from './pages/Detail'
+import Home from './pages/Home'
+import Players from './pages/Players'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path='/' element={ <Home />} />
+          <Route path='/jugadores' element={ <Players />} />
+          <Route path='/detail' element={ <Detail />} />
+          <Route path='/nosotros' element={ <About />} />
+          <Route path='/contacto' element={ <Contact />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  )
+}
+
+export default App
