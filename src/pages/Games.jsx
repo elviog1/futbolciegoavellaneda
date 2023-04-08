2aa05d7236d503c0839d321710dd0aa381b8de2b
Elvio Galeano <elvio.galeano827@gmail.com>
2023-04-08 16:11:52 -0300

    host img

--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -22,7 +22,7 @@ const api = "https://futbolciegoavellaneda.onrender.com/games"
 
         {loading ?
         <div className='mt-5 sm:w-8/12'>
-          <img className='w-full h-96 object-cover' src='https://scontent.feze12-1.fna.fbcdn.net/v/t39.30808-6/339742060_603163724767954_1633234389782434072_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=730e14&_nc_ohc=TbydGVQaWOQAX91pYT6&_nc_ht=scontent.feze12-1.fna&oh=00_AfAubr9aHYVZhfOj-NieadXjmLCxtYVyWAjG9dcLzZb69g&oe=643055D8' />
+          <img className='w-full h-96 object-cover' src='https://i.imgur.com/wTjlbjX.jpg' />
           <h1 className='sm:text-4xl text-4xl text-center text-gray-300 font-bold relative bottom-44  tracking-widest  uppercase backdrop-blur-lg'>Copa Claudio Monzón</h1>
           <RankingTeam games={games} />
           <div className='h-96 overflow-y-auto mt-4'>
